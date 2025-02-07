0fc31fb97cbc56ed80dc59d8656ecba66b266809
Bavishya04 <bavishyanarayanan123@gmail.com>
2025-02-07 15:25:53 +0530

    Add files via upload

---
+++ qn4/en.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+int i,n;
+printf("Enter the value of n:");
+scanf("%d",&n);
+ printf("Even numbers from 1to 100:\n");
+ for (i=2;i<=n;i+=2)
+ {printf("%d\n",i);
+ }
+ printf("\n");
+ return 0;
+ }
