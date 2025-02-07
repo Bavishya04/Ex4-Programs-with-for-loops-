f5f32291c46f49588340c766a4e2f43a43d74d8e
Bavishya04 <bavishyanarayanan123@gmail.com>
2025-02-07 15:23:40 +0530

    Add files via upload

---
+++ qn3/on.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+int i,n;
+printf("Enter the value of n:");
+scanf("%d",&n);
+ printf("Odd numbers from 1to 100:\n");
+ for (i=1;i<=n;i+=2)
+ {printf("%d\n",i);
+ }
+ printf("\n");
+ return 0;
+ }
